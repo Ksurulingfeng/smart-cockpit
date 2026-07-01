89747636c0fd232daaedeffb2cbd840b9fd4e5cb
Ksurulingfeng <a2365127723@outlook.com>
2026-07-01 23:16:45 +0800

    fix: uds_handler.h 补回 UDS_RESP_ID 定义 (引用 uds_protocol.h)

--- stm32_can_node/Tasks/Inc/uds_handler.h
+++ stm32_can_node/Tasks/Inc/uds_handler.h
@@ -2,6 +2,13 @@
 #define UDS_HANDLER_H
 
 #include "sys.h"
+#include "../../../shared/uds_protocol.h"
+
+#ifdef NODE_A
+#define UDS_RESP_ID CAN_ID_UDS_RESP_A
+#else
+#define UDS_RESP_ID CAN_ID_UDS_RESP_B
+#endif
 
 void Uds_HandleRequest(uint8_t *data, uint8_t len);
 void Uds_CheckS3Timeout(void);
